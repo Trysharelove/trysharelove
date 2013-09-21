6e39000d41971120ae1c87d0406981356fb293fc
steveholm <steve.m.holm@gmail.com>
2013-09-21 11:20:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trysharelove::Application.config.secret_key_base = 'e98ec56a19f8c8588b8cab03c2d60e6e160ff3b936378fbaa65070e795cd2b5b8c079bac9b93424ab133b479698dd0e5c15003db19c0cb5daa5b1b50f1d74e94'
