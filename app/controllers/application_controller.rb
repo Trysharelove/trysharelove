343947171ec1080234d2c8164101bc15f0da6503
steveholm <steve.m.holm@gmail.com>
2013-09-21 11:34:55 -0600

    testing new appname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-	  render text: "Hello World"
+	  render text: "Hello People"
 	end
 
 end
